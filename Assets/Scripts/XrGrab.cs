1ee00f0e7984959806dc1a9b1747cbc052bab2f1
Rich <rsbeare@gmail.com>
2019-10-28 17:08:32 -0400

    End of Session 9

---
+++ Assets/Scripts/XrGrab.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XrGrab : MonoBehaviour
+{
+    public GameObject m_CollidingObject;
+    public GameObject m_HeldObject;
+
+    public Animator m_animator; // This is a reference to the animator component on the sim hand
+
+    //XR-specific variables
+    public string gripInputName; // Name of the grip thjat I set up in the Project Settings Inputs list
+
+    private bool gripHeld; 
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>())
+        {
+            m_CollidingObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_CollidingObject = null;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis(gripInputName) > 0.5f && gripHeld == false) // Grab
+        {
+            //m_animator.SetBool("Close Hand", true); // Changes the parameter in the Animator
+
+            if (m_CollidingObject)
+            {
+                Grab();
+            }
+
+            gripHeld = true; // Prevent from running this code (e.g. m_animator.SetBool("Close Hand", true) and Grab()) every frame (e.g. 90x per sec)
+        }
+        else if (Input.GetAxis(gripInputName) < 0.5f && gripHeld == true) // Release
+        {
+            //m_animator.SetBool("Close Hand", false); // Changes the parameter in the Animator
+
+            if (m_HeldObject)
+            {
+                Release();
+            }
+
+            gripHeld = false; // No longer holding the grip, so only let this code in "else if" statement run once upon release of grip
+        }
+    }
+
+    void Grab()
+    {
+        m_HeldObject = m_CollidingObject;
+        m_HeldObject.GetComponent<Rigidbody>().isKinematic = true;
+        m_HeldObject.transform.SetParent(transform);
+    }
+
+    void Release()
+    {
+        m_HeldObject.transform.SetParent(null);
+        m_HeldObject.GetComponent<Rigidbody>().isKinematic = false;
+        m_HeldObject = null;
+    }
+}
---
+++ Assets/XrTeleport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XrTeleport : MonoBehaviour
+{
+    public Transform xrRig; // Need a reference to my XrRig, since that's what I'm moving!
+    public LineRenderer line; // Need reference to line renderer to have it point where I am pointing
+    public string teleportButtonName; // Need a axis name, so let's make that a public string so we can choose left or right hand
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(0, this.transform.position); // We need to define the start point of the line every frame, becuase our hand moves freely
+
+        // We need to "raycast" sense the trajectory of the laser, i.e. where we're pointing
+        RaycastHit hit; // This will hold information about what we're pointing at, and the position that is hit by our "laser"
+
+        
+
+    }
+}
