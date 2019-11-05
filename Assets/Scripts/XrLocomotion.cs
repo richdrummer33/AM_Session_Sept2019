f269670bed3f85c7f6944667fd4760ac5f8bb6e5
Rich <rsbeare@gmail.com>
2019-11-04 20:27:24 -0500

    End of session 11

--- Assets/Scripts/XrLocomotion.cs
+++ Assets/Scripts/XrLocomotion.cs
@@ -8,6 +8,8 @@ public class XrLocomotion : MonoBehaviour
 
     public Transform xrRig; // Moving rig, just like in teleport!
 
+    public LayerMask raycastMask; // Define what layers to "sense" floor height
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +29,25 @@ public class XrLocomotion : MonoBehaviour
 
         // Apply motion now to the rig!
         xrRig.transform.position = xrRig.transform.position + (forward + right) * Time.deltaTime;
+
+        // Terrain-height adjustment:
+        float floorHeight = GetFloorHeight(); // Get the floor height
+        Vector3 rigPosition = xrRig.position; // Take note of the current rig positon, so we can edit the y-value 
+        rigPosition.y = floorHeight; // Change the y-value
+        xrRig.transform.position = rigPosition; // Now we have the xrRig position with the y-value updated, so apply it back to the rig's position
+    }
+
+    private float GetFloorHeight()
+    {
+        float floorHeight = 0f; // Will hold the value of the heioght as sensed by raycast
+
+        RaycastHit hitInfo; // Store info about my raycast hit (incliding position -- has height info)
+        
+        if(Physics.Raycast(Camera.main.transform.position, Vector3.down, out hitInfo, Mathf.Infinity, raycastMask)) // Perform a raycast from my head down to the ground 
+        {
+            floorHeight = hitInfo.point.y; // Take note of the floor height (height of surface that was hit)
+        }
+
+        return floorHeight; // Give this value to the caller 
     }
 }
